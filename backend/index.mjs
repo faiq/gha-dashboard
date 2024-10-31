411066af2edd10257e4968104492bebb2517ba18
faiq <faiq.raza@nutanix.com>
2024-10-31 14:28:52 -0700

    fix: allow cors in server

--- backend/index.mjs
+++ backend/index.mjs
@@ -5,10 +5,21 @@ import fetch from 'node-fetch';
 import session from 'express-session';
 import cors from 'cors';
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200
+var whitelist = ['http://localhost:3000', 'http://localhost:8080']; //white list consumers
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
 };
+
 
 const sessionOptions = {
   secret: process.env.SESSION_ID_SECRET || 'iamabanana',
