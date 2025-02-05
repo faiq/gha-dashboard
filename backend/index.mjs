2d97ee4417dd3d379c0f8dcf4bb1e00a218f3662
faiq <faiq.raza@nutanix.com>
2025-02-05 10:15:13 -0800

    fix: reorder middleware

--- backend/index.mjs
+++ backend/index.mjs
@@ -39,10 +39,10 @@ const cookieSessionOptions = {
 const app = express();
 
 app.set('trust proxy', 1) // trust first proxy
+app.use(cookieSession(cookieSessionOptions));
 app.use(cors(corsOptions));
 app.set('port', process.env.PORT || 3001);
 app.use(bodyParser.json());
-app.use(cookieSession(cookieSessionOptions));
 
 app.get('/healthz', function(req, res) {
   res.send('ok');
